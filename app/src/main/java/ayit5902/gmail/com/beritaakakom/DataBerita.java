623ea8b15bd194c32db466c03e752c5446793c1f
tarisiddiq <ayit5902@gmail.com>
2018-12-25 21:32:05 -0800

    Pertemuan 13 commit

---
+++ app/src/main/java/ayit5902/gmail/com/beritaakakom/DataBerita.java
@@ -0,0 +1,8 @@
+package ayit5902.gmail.com.beritaakakom;
+
+public class DataBerita {
+    public String judulBerita;
+    public String isiBerita;
+    public String tglPosting;
+    public String gambarBerita;
+}
